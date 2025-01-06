d03371cf6e42760d88cd5955a40ad7ebb6fce6d3
srivalli537 <srivallisormar0408@gmail.com>
2025-01-06 16:45:04 +0530

    pulling

---
+++ app.js
@@ -0,0 +1,21 @@
+let str1 = "double quotes";
+console.log(str1 , typeof(str1));
+
+let str2 = 'single quote';
+console.log(str2 , typeof(str2));
+
+let bool1 = true;
+console.log(boll1, typeof(bool1));
+
+let bool2 = false;
+console.log(bool2 , typeof(bool2));
+
+let num1 = 10;
+console.log(num1 , typeof(num1));
+let num2 = 10.2345;
+console.log(num2 , typeof(num2));
+
+let num3 = 10e3;
+console.log(num3 , typeof(num3));
+
+
